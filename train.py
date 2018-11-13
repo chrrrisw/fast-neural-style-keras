e2a3a96db8815c3466098796d1d10fc9e1127872
Chris Willoughby <chrrrisw@gmail.com>
2018-11-13 18:33:08 +1030

    add command line args for train_image_path, save_interval and model resume, rename display_img to save_img (that's what it does), base max_images on number of images, catch keyboard interrupt, save model when finished

--- train.py
+++ train.py
@@ -7,11 +7,12 @@ from scipy.misc import imsave
 import time
 import numpy as np
 import argparse
+import os
 
 import nets
 
 
-def display_img(i, x, style, is_val=False):
+def save_img(i, x, style, is_val=False):
     # save current generated image
     img = x  # deprocess_image(x)
     if is_val:
@@ -49,9 +50,16 @@ def main(args):
     )
     model.summary()
 
-    nb_epoch = 82785 * 2
+    # TODO: Cope with other file extensions
+    num_images = 0
+    for root, dirs, files in os.walk(args.train_image_path, followlinks=True):
+        # print(root, dirs)
+        num_images += len([f for f in files if f.endswith("jpg")])
+
+    max_images = num_images * 2
+    print("Number of images to be processed:", max_images)
+
     train_batchsize = 1
-    train_image_path = "images/train/"
 
     # learning_rate = 1e-3  # 1e-3
     optimizer = Adam()  # Adam(lr=learning_rate,beta_1=0.99)
@@ -66,44 +74,56 @@ def main(args):
         (train_batchsize, img_width, img_height, 3)
     )  # Dummy output, not used since we use regularizers to train
 
-    # model.load_weights(style+'_weights.h5',by_name=False)
+    # TODO: Start where we left off
+    # model.load_weights(style + '_weights.h5', by_name=False)
+    if args.resume[0] is not None:
+        print(f"Resuming from {args.resume[1]}")
+        model.load_weights(args.resume[0])
+        skip_to = int(args.resume[1])
+    else:
+        skip_to = 0
 
-    skip_to = 0
-
-    i = 0
+    image_count = 0
     t1 = time.time()
-    for x in datagen.flow_from_directory(
-        train_image_path,
-        class_mode=None,
-        batch_size=train_batchsize,
-        target_size=(img_width, img_height),
-        shuffle=False,
-    ):
-        if i > nb_epoch:
-            break
 
-        if i < skip_to:
-            i += train_batchsize
-            if i % 1000 == 0:
-                print("skip to: %d" % i)
+    try:
+        pass
+        for x in datagen.flow_from_directory(
+            args.train_image_path,
+            class_mode=None,
+            batch_size=train_batchsize,
+            target_size=(img_width, img_height),
+            shuffle=False,
+        ):
+            # We're done
+            if image_count > max_images:
+                break
 
-            continue
+            # Skip training until image_count >= skip_to
+            if image_count < skip_to:
+                image_count += train_batchsize
+                if image_count % 1000 == 0:
+                    print("skip to: %d" % image_count)
 
-        hist = model.train_on_batch(x, dummy_y)
+                continue
 
-        if i % 50 == 0:
-            print(hist, (time.time() - t1))
-            t1 = time.time()
+            hist = model.train_on_batch(x, dummy_y)
 
-        if i % 500 == 0:
-            print("epoch: ", i)
-            val_x = net.predict(x)
+            if image_count % args.save_interval == 0:
+                print(image_count, hist, (time.time() - t1))
+                val_x = net.predict(x)
+                save_img(image_count, x[0], style)
+                save_img(image_count, val_x[0], style, True)
+                model.save_weights(style + "_weights.h5")
+                t1 = time.time()
 
-            display_img(i, x[0], style)
-            display_img(i, val_x[0], style, True)
-            model.save_weights(style + "_weights.h5")
+            image_count += train_batchsize
 
-        i += train_batchsize
+    except KeyboardInterrupt:
+        print("Stopped by user.")
+
+    # Save the final model
+    model.save_weights(f"{style}_weights_final_{image_count}.h5")
 
 
 if __name__ == "__main__":
@@ -134,6 +154,9 @@ if __name__ == "__main__":
     parser.add_argument("--content_weight", default=1.0, type=float)
     parser.add_argument("--style_weight", default=4.0, type=float)
     parser.add_argument("--image_size", default=256, type=int)
+    parser.add_argument("--train_image_path", default="images/train/", type=str)
+    parser.add_argument("--save_interval", default=50, type=int)
+    parser.add_argument("--resume", nargs=2, default=[None, 0], help="Model name and iteration number")
 
     args = parser.parse_args()
     main(args)
